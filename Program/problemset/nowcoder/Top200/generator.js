df113a7348ff15dc5a43bc200d2e1e623ab127da
kenblikylee <kenblikylee@126.com>
2021-05-27 12:41:22 +0800

    update

--- Program/problemset/nowcoder/Top200/generator.js
+++ Program/problemset/nowcoder/Top200/generator.js
@@ -2,7 +2,7 @@ const data = require('./top200.json')
 const fs = require('fs')
 const pairs = require('./tags')
 
-let out = data.map((item, index) => (`## ${index+1}. [${item.title}](${item.url})\n\n${item.content}\n\n### Tags:\n${item.tags.map(t => pairs.get(t) ? `- [${t}](https://leetcode-cn.com/tag/${pairs.get(t)}/problemset/)` : `- ${t}`).join('\n')}`)).join('\n\n')
+let out = data.map((item, index) => (`### ${index+1}. [${item.title}](${item.url})\n\n${item.content}\n\n#### Tags:\n${item.tags.map(t => pairs.get(t) ? `- [${t}](https://leetcode-cn.com/tag/${pairs.get(t)}/problemset/)` : `- ${t}`).join('\n')}`)).join('\n\n')
 
 let tagCount = new Map()
 data.forEach(item => {
@@ -24,4 +24,4 @@ for (let entry of tagCount.entries()) {
 let sorted = tagItems.sort((a, b) => b.items.length - a.items.length)
 let tagsRank = sorted.map(item => `- [${item.tag} (${item.items.length})](https://leetcode-cn.com/tag/${pairs.get(item.tag)}/problemset/)\n${item.items.map(t => `\t- [${t[0]}](${t[1]})`).join('\n')}`).join('\n')
 
-fs.writeFileSync('./README.md', '# 字节跳动笔试\n\n' + tagsRank + '\n\n' + out + '\n', 'utf8')
+fs.writeFileSync('./README.md', '# 字节跳动笔试\n\n## 标签统计\n\n' + tagsRank + '\n\n## 热门题目\n\n' + out + '\n', 'utf8')
