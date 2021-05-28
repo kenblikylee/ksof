94a54c6c7d1cd9f8861f1a863ad5eb3e5b8d07aa
kenblikylee <42268955+kenblikylee@users.noreply.github.com>
2021-05-28 09:55:18 +0800

    Update 1.js

--- Program/problemset/nowcoder/Top200/solutions/4 linked-list/1.js
+++ Program/problemset/nowcoder/Top200/solutions/4 linked-list/1.js
@@ -1,2 +1,22 @@
 // #1 反转链表
 // 输入一个链表，反转链表后，输出新链表的表头。
+
+/*function ListNode(x){
+    this.val = x;
+    this.next = null;
+}*/
+function ReverseList(pHead)
+{
+    // write code here
+    if (!pHead) return null
+    var pre = null
+    var cur = pHead
+    var next
+    while (cur) {
+        next = cur.next
+        cur.next = pre
+        pre = cur
+        cur = next
+    }
+    return pre
+}
